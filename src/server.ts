923d3dbea6520b940fca9262b61fd9669f77fcdf
Emanuel Soares <santindosanjos@gmail.com>
2025-04-16 02:13:27 -0300

    Vai dar certo

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,9 @@ import fileUpload from 'express-fileupload';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(fileUpload({
-  limits:{fileSize: 50 * 1024 * 1024}
-}))
+//app.use(fileUpload({
+  //limits:{fileSize: 50 * 1024 * 1024}
+//}))
 app.use(router);
 
 app.use(
